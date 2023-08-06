7be9cf55e569426ac0912b01b3c19f10b92a1675
Angelo Ryan Pabualan <ryanpabz16@gmail.com>
2023-08-06 20:45:32 +0800

    Fix linking SFML libraries

---
+++ Game.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Mouse.hpp>
+
+using namespace sf;
+
+class Game {
+	const int FPS = 60;
+	const Color boardWhite = Color(213, 207, 191);
+	const Color borderRed = Color(134, 28, 24);
+	const Color snakeBlack = Color(23, 31, 33);
+	const Color foodGreen = Color(1, 152, 98);
+	const std::string title = "BamBamBam";
+	const float speed = 300;
+	const float width = 1080, height = 720;
+	int thickness;
+	int delayTurn;
+
+	RenderWindow window;
+	RectangleShape borders[4];
+
+	Font quicksand;
+	Font rocksalt;
+	Text scoreTxt;
+
+	RectangleShape startButton;
+	Text startTxt;
+
+	RectangleShape s;
+	std::vector<RectangleShape> snake;
+	std::vector<Vector2f> snakePath;
+	std::string direction;
+	std::string current;
+	int growth, scoreInt, delay;
+
+	CircleShape food;
+
+	Clock clock;
+	float dt;
+	bool playing;
+
+	RectangleShape& snakeHead();
+	void displayStart();
+	void createBorders();
+	void displayScore();
+	void createSnake();
+	void createFood();
+
+	void wiggle();
+	void eat();
+
+	void initWindow();
+	void initPlay();
+	void windowCloser();
+	void mainMenu();
+	void movement();
+	void collision();
+	void render();
+
+public:
+	Game();
+	void run();
+	void start();
+};
